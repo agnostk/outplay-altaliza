e62e61c37a40f23f38410c684772418046f9ac38
Felipe Cardoso <felipehasc@gmail.com>
2021-09-22 19:56:31 -0300

    feat(DAL): added categories migration

--- backend/Altaliza.DAL/AppDbContext.cs
+++ backend/Altaliza.DAL/AppDbContext.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Altaliza.Core.Entities;
 using Altaliza.DAL.Constraints;
 using Microsoft.EntityFrameworkCore;
@@ -19,6 +20,20 @@ namespace Altaliza.DAL
             modelBuilder.Entity<Vehicle>(new VehicleConstraints().Configure);
             modelBuilder.Entity<Category>(new CategoryConstraints().Configure);
             modelBuilder.Entity<CharacterVehicle>(new CharacterVehicleConstraints().Configure);
+
+            modelBuilder.Entity<Category>().HasData(new List<Category>
+            {
+                new Category {Id = 1, Name = "Commercial", Description = "Comerciais"},
+                new Category {Id = 2, Name = "Compact", Description = "Compactos"},
+                new Category {Id = 3, Name = "Military", Description = "Militares"},
+                new Category {Id = 4, Name = "Motorcycle", Description = "Motos"},
+                new Category {Id = 5, Name = "Off-Road", Description = "Off-Roads"},
+                new Category {Id = 6, Name = "Sedan", Description = "Sedans"},
+                new Category {Id = 7, Name = "Sport", Description = "Esportivos"},
+                new Category {Id = 8, Name = "Super", Description = "Superesportivos"},
+                new Category {Id = 9, Name = "SUV", Description = "SUVs"},
+                new Category {Id = 10, Name = "Van", Description = "Vans"},
+            });
         }
     }
 }
---
+++ backend/Altaliza.DAL/Migrations/20210922225126_AddedCategories.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Altaliza.DAL.Migrations
+{
+    public partial class AddedCategories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Category",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Comerciais", "Commercial" },
+                    { 2, "Compactos", "Compact" },
+                    { 3, "Militares", "Military" },
+                    { 4, "Motos", "Motorcycle" },
+                    { 5, "Off-Roads", "Off-Road" },
+                    { 6, "Sedans", "Sedan" },
+                    { 7, "Esportivos", "Sport" },
+                    { 8, "Superesportivos", "Super" },
+                    { 9, "SUVs", "SUV" },
+                    { 10, "Vans", "Van" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: 10);
+        }
+    }
+}
